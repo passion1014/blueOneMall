3ee56186ee2a74f2261b134adf5d194856fe47a1
dhkim0279 <dhkim0279@gmail.com>
2014-05-02 17:21:33 +0900

    dana

--- src/main/java/com/blueone/common/util/FileUploadUtility.java
+++ src/main/java/com/blueone/common/util/FileUploadUtility.java
@@ -36,7 +36,7 @@ public class FileUploadUtility {
 //	public final static String FILE_UPLOAD_DIR = "src/main/webapp/uploads";
 	//public final static String FILE_UPLOAD_DIR = "src/main/webapp/uploads";
 
-	public final static String FILE_UPLOAD_DIR = "src/main/webapp";
+	public final static String FILE_UPLOAD_DIR = "tomcat/webapps/ROOT/upload";
 //	public final static String FILE_UPLOAD_DIR = "C:/Users/dh-Kim/Documents/GitHub/blueOneMall/src/main/webapp/resources/upload";
 
 
